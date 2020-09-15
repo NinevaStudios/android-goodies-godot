f0ff8587eb7919c1c77be1a45225dd3fabdb6204
Andriy Marshalek <amarshalek@ninevastudios.com>
2020-09-15 16:00:19 +0300

    add confirmation dialog method to demo

--- Plugin/AndroidGoodies/Demo/DemoNativeUi.gd
+++ Plugin/AndroidGoodies/Demo/DemoNativeUi.gd
@@ -3,5 +3,11 @@ extends Node
 const NativeUiClass = preload("../Scripts/AGNativeUi.gd")
 
 func _onShowToastButtonClick():
-	print("Button clicked")
 	NativeUiClass.showToast("Toast", 0)
+
+
+func _onShowConfirmationDialogButtonClick():
+	NativeUiClass.showConfirmationDialog("Confirmation Dialog", "Do you accept your swift death?", "Okay...", "_onPositiveButtonClicked", self)
+	
+func _onPositiveButtonClicked():
+	NativeUiClass.showToast("Positive Button Clicked", 0)
--- Plugin/AndroidGoodies/Scripts/AGNativeUi.gd
+++ Plugin/AndroidGoodies/Scripts/AGNativeUi.gd
@@ -8,3 +8,11 @@ static func showToast(text, length):
 		singleton.showToast(text, length)
 	else:
 		print("No plugin singleton")
+		
+static func showConfirmationDialog(title, body, button_text, button_callback_name, button_callback_object):
+	if Engine.has_singleton(plugin_name):
+		var singleton = Engine.get_singleton(plugin_name)
+		singleton.connect("onConfirmButtonClicked", button_callback_object, button_callback_name)
+		singleton.showConfirmationDialog(title, body, button_text)
+	else:
+		print("No plugin singleton")
