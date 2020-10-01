ec2dfe0c03527cd6918967b11b80f2e9777486ca
Andriy Marshalek <amarshalek@ninevastudios.com>
2020-10-01 15:23:17 +0300

    add sharing API methods

--- Plugin/AndroidGoodies/Scripts/AGShare.gd
+++ Plugin/AndroidGoodies/Scripts/AGShare.gd
@@ -1,16 +1,87 @@
-extends Node
+class_name AGShare
 
+func share_text(subject : String, text_body : String, 
+		with_chooser : bool = false, chooser_title : String = ""):
+	if Engine.has_singleton(AGUtils.plugin_name):
+		var singleton = Engine.get_singleton(AGUtils.plugin_name)
+		singleton.shareText(subject, text_body, with_chooser, chooser_title)
+	else:
+		print("No plugin singleton")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func share_image(image : Image, with_chooser : bool = false, chooser_title : String = ""):
+	if Engine.has_singleton(AGUtils.plugin_name):
+		var singleton = Engine.get_singleton(AGUtils.plugin_name)
+		var image_path = _save_image_to_cache(image)
+		singleton.shareImage(image_path, with_chooser, chooser_title)
+	else:
+		print("No plugin singleton")
 
+func share_text_with_image(subject : String, text_body : String, image : Image,
+		with_chooser : bool = false, chooser_title : String = ""):
+	if Engine.has_singleton(AGUtils.plugin_name):
+		var singleton = Engine.get_singleton(AGUtils.plugin_name)
+		var image_path = _save_image_to_cache(image)
+		singleton.shareTextWithImage(subject, text_body, image_path, with_chooser, chooser_title)
+	else:
+		print("No plugin singleton")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func share_video(video_path : String, with_chooser : bool = false, chooser_title : String = ""):
+	if Engine.has_singleton(AGUtils.plugin_name):
+		var singleton = Engine.get_singleton(AGUtils.plugin_name)
+		singleton.shareVideo(video_path, with_chooser, chooser_title)
+	else:
+		print("No plugin singleton")
 
+func send_sms_via_messaging_app(phone_number : String, message : String, 
+		with_chooser : bool = false, chooser_title : String = ""):
+	if Engine.has_singleton(AGUtils.plugin_name):
+		var singleton = Engine.get_singleton(AGUtils.plugin_name)
+		singleton.sendSmsViaMessagingApp(phone_number, message, with_chooser, chooser_title)
+	else:
+		print("No plugin singleton")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func send_sms_directly(phone_number : String, message : String):
+	if Engine.has_singleton(AGUtils.plugin_name):
+		var singleton = Engine.get_singleton(AGUtils.plugin_name)
+		singleton.sendSmsDirectly(phone_number, message)
+	else:
+		print("No plugin singleton")
+
+func send_email(subject : String, text_body : String, image : Image, recipients : PoolStringArray, 
+		cc : PoolStringArray, bcc : PoolStringArray, with_chooser : bool = false, chooser_title : String = ""):
+	if Engine.has_singleton(AGUtils.plugin_name):
+		var singleton = Engine.get_singleton(AGUtils.plugin_name)
+		var image_path = _save_image_to_cache(image)
+		singleton.sendEMail(subject, text_body, image_path, recipients, cc, bcc, with_chooser, chooser_title)
+	else:
+		print("No plugin singleton")
+
+func send_email_with_images(subject : String, extra_images : Array, recipients : PoolStringArray, 
+		cc : PoolStringArray, bcc : PoolStringArray, with_chooser : bool = false, chooser_title : String = ""):
+	if Engine.has_singleton(AGUtils.plugin_name):
+		var singleton = Engine.get_singleton(AGUtils.plugin_name)
+		
+		var image_paths = PoolStringArray()
+		for image in extra_images:
+			var image_path = _save_image_to_cache(image)
+			image_paths.append(image_path)
+		
+		singleton.sendEMail(subject, image_paths, recipients, cc, bcc, with_chooser, chooser_title)
+	else:
+		print("No plugin singleton")
+		
+func _save_image_to_cache(image : Image) -> String:
+	if image == null or not image is Image:
+		print("Image is not valid. Not saving")
+		return ""
+
+	if Engine.has_singleton(AGUtils.plugin_name):
+		var singleton = Engine.get_singleton(AGUtils.plugin_name)
+		
+		image.decompress()
+		image.convert(Image.FORMAT_RGBA8)
+		
+		return singleton.saveImageToCache(image.get_data(), image.get_width(), image.get_height())
+	else:
+		print("No plugin singleton")
+		return ""
